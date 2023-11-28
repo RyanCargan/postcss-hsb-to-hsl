939231d3f609da54b7ad3baa15c0ca595d42b3f8
Ryan <ryancargan@gmail.com>
2023-11-28 22:40:25 +0530

    Modify converter to test bugfix

--- index.js
+++ index.js
@@ -3,28 +3,34 @@
  */
 
 const convertHSBToHSL = (h, s, b, a = 1) => {
-	const l = ((2 - s) * b) / 2
-	return `hsla(${h}, ${s}%, ${l}%, ${a})`
+	const lightness = ((2 - s / 100) * b) / 2
+	let saturation = s * b
+	if (lightness > 0 && lightness <= 50) {
+		saturation = saturation / (100 - lightness * 2)
+	} else if (lightness > 50) {
+		saturation = saturation / (2 * lightness)
+	}
+	return `hsla(${h}, ${saturation}%, ${lightness}%, ${a})`
 }
 
-// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-module.exports = (opts = {}) => {
+module.exports = () => {
 	return {
 		postcssPlugin: 'postcss-hsb-to-hsl',
 		Once(root) {
 			root.walkDecls((decl) => {
-				const hsbRegex = /hsb\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*(,\s*(\d+(\.\d+)?)%?\s*)?\)/g
+				const hsbRegex =
+					/hsb(a)?\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*(,\s*(\d+(\.\d+)?)%?\s*)?\)/g
 				const hsbValues = decl.value.match(hsbRegex)
 
 				if (hsbValues) {
 					hsbValues.forEach((hsbValue) => {
 						const matches = hsbValue.match(
-							/hsb\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*(,\s*(\d+(\.\d+)?)%?\s*)?\)/
+							/hsb(a)?\(\s*(\d+)\s*,\s*(\d+)%\s*,\s*(\d+)%\s*(,\s*(\d+(\.\d+)?)%?\s*)?\)/
 						)
-						const h = parseInt(matches[1], 10)
-						const s = parseInt(matches[2], 10)
-						const b = parseInt(matches[3], 10)
-						const a = parseFloat(matches[5]) || 1
+						const h = parseInt(matches[2], 10)
+						const s = parseInt(matches[3], 10)
+						const b = parseInt(matches[4], 10)
+						const a = matches[6] ? parseFloat(matches[6]) : 1
 						const hslValue = convertHSBToHSL(h, s, b, a)
 						decl.value = decl.value.replace(hsbValue, hslValue)
 					})
